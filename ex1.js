915ab608af46fcb813ef703ce91b36cce2f931f1
Ibrahim Sowunmi <i.sowunmi97@gmail.com>
2019-10-14 21:02:46 +0100

    Completed exercise 1 - 11

---
+++ ex1.js
@@ -0,0 +1,11 @@
+// Traversing an Array
+// Exercise 1: Print all the names in an array
+
+function(console) {
+    var names = ["Ben", "Jafar", "Matt", "Priya", "Brian"],
+        counter;
+
+    for (counter = 0; counter < names.length; counter++) {
+        console.log(names[counter]);
+    }
+}
