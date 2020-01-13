af51dd13bb9c83746a2748f538124d08823c0493
Ibrahim Sowunmi <i.sowunmi97@gmail.com>
2020-01-13 23:20:30 +0000

    learning more

---
+++ rithim-ex/ex10.js
@@ -0,0 +1,11 @@
+var name = "Josie"
+console.log(`Print all of ${name}`);
+
+const DO_NOT_CHANGE = 42;
+
+var arr = [1,2];
+arr.reverse();
+
+const double = arr => arr.map(val => val * 2);
+
+const add = ( a = 0, b = 0 ) => a + b;
